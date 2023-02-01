e547b9c75ae471bca3ea0ada31ce0a66ae9188b3
daneltor <daneltor@espol.edu.ec>
2023-01-31 20:22:07 -0500

    P&V de preguntas 1,2,3 de Daniel Torres

--- P&V-Daniel Torres/main.py
+++ P&V-Daniel Torres/main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
 
-df = pd.read_csv("steamGames.csv", 
+df = pd.read_csv("P&V-Daniel Torres\steamGames.csv",
                  names=["Nombre", "Precio", "Fecha", "Lenguajes", "Desarrollador", "Generos", "Rating", "Link"],
                  header=0)
 
@@ -11,7 +12,7 @@ for index, row in df.iterrows():
 
 
 #¿Cuántos juegos tienen una valoración mayor al 50% y una valoración menor al 50%?
-def quanti_val(df):
+def pregunta1(df):
     higher = 0
     lower = 0
 
@@ -34,4 +35,74 @@ def quanti_val(df):
 
     plt.show()
 
-quanti_val(df)
+
+#¿Cuáles son los 10 juegos mejor puntuados del género de Supervivencia/Accion?
+def pregunta2(df):
+    df = df.sort_values("Rating",ascending=False)
+    values = list()
+    labels = list()
+    count = 0
+    for index, row in df.iterrows():
+        
+        if count == 10:
+            break
+        
+        genre = str(row["Generos"])
+        if genre != "nan":
+            print("genero")
+            print(genre)
+
+            if genre.lower().find("action"):                
+                labels.append(row["Nombre"])
+                values.append(row["Rating"])
+                count += 1
+    
+    print(labels)
+    print(values)
+    plt.bar(labels,values)
+    for i, v in enumerate(values):
+        plt.text(i - 0.25, v + 0.5, str(v))
+    plt.show()
+
+
+#¿Cuántos juegos fueron lanzados por año (en orden descendente)?
+def pregunta3(df):
+
+    year_dict = dict()
+
+    for index, row in df.iterrows():
+
+        print('fecha')
+        print(row['Fecha'])
+
+        result = re.search(r'\d{4}', row['Fecha'])
+        if result:
+
+            year = result.group()
+
+            if year in year_dict.keys():
+                year_dict[year] +=1
+            else:
+                year_dict[year] = 1
+
+    print(year_dict)
+    
+    sorted_dict = sorted(year_dict.items(), key=lambda x: x[1], reverse=True)
+    year_dict = dict(sorted_dict)
+
+    labels = list(year_dict.keys())
+    values = list(year_dict.values())
+
+    plt.bar(labels,values)
+    for i, v in enumerate(values):
+        plt.text(i - 0.25, v + 0.5, str(v))
+    plt.show()
+
+
+
+# 3 funciones para las preguntas 1, 2, y 3 repectivamente segun la reparticion de preguntas.
+
+# pregunta1(df) 
+# pregunta2(df)
+# pregunta3(df)
+
